5553906b26e51e5b5cacaa290cc5e934978f9aae
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-04-18 21:13:38 +0200

    feat: Fetch data

---
+++ src/helpers/request.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const request = axios.create({
+  baseURL:
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false",
+});
+
+export default request;
